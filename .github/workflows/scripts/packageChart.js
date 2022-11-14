1d7f9abe3b96673d9c28a7286543a2a9f7b21eef
Ljubo Nikolic <ljubo.nikolic@neon-free.ch>
2022-11-14 15:23:55 +0000

    add librenms source code, fix try-catch

--- .github/workflows/scripts/packageChart.js
+++ .github/workflows/scripts/packageChart.js
@@ -43,28 +43,30 @@ module.exports = async ({ core, exec }) => {
           core.notice(`${checkoutSourceDir}/${chart.source}/${file}\n Version: ${version}`)
           return core.setFailed(`The file ${checkoutSourceDir}/${chart.source}/${file} doesn't exist\n${error}`);
         }
-
-        await exec.exec('helm', ['package', `${checkoutSourceDir}/${chart.source}`, '-d', `${checkoutPageDir}/${chart.destination}`])
-      } else {
-        core.notice(`Packaging the chart ${chart.name}`)
-        await exec.exec('helm', ['package', `${checkoutSourceDir}/${chart.source}`, '-d', `${checkoutPageDir}/${chart.destination}`])
       }
+
+      core.notice(`Packaging the chart ${chart.name}`)
+      await exec.exec('helm', ['package', `${checkoutSourceDir}/${chart.source}`, '-d', `${checkoutPageDir}/${chart.destination}`])
       await exec.exec('git', ['add', `${chart.destination}`], { cwd: checkoutPageDir })
     }
   } catch (error) {
     console.error(error);
-    return core.setFailed(`Unable to update the version and stage files with error: ${error}`)
+    return core.setFailed(`Unable to create package and stage it with an error: ${error}`)
   }
 
-  let status = "";
-  await exec.exec('git', ['status', '--porcelain=v1'], {
-    cwd: checkoutPageDir,
-    listeners: { 'stdout': data => { status += data.toString() }}
-  })
-  for (const line of status.split('\n')) {
-    if (line.length > 0 && !line.startsWith('A')) {
-      return core.setFailed(`Only new files to be added, but that's not the case: ${line}`)
+  try {
+    let status = "";
+    await exec.exec('git', ['status', '--porcelain=v1'], {
+      cwd: checkoutPageDir,
+      listeners: { 'stdout': data => { status += data.toString() }}
+    })
+    for (const line of status.split('\n')) {
+      if (line.length > 0 && !line.startsWith('A')) {
+        return core.setFailed(`Only new files to be added, but that's not the case: ${line}`)
+      }
     }
+  } catch (error) {
+      return core.setFailed(`Unable to validate the git status with error ${error}`)
   }
 
 }
