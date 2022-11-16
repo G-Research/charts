524dc7b9cbe8fa4a7b3d6b6eda04256d9c314567
Ljubo Nikolic <ljubonikolic@gmail.com>
2022-11-16 12:33:25 +0000

    clean code - improve names, types, validation

--- .github/workflows/scripts/validateChart.js
+++ .github/workflows/scripts/validateChart.js
@@ -1,6 +1,6 @@
 module.exports = async ({ core, context }) => {
-  const source = new Map(Object.entries(require('./config.json')))
-  var input = (context.eventName == "workflow_dispatch") ? {
+  const config = new Map(Object.entries(require('./config.json')))
+  var input = (context.eventName === "workflow_dispatch") ? {
     owner: context.payload.inputs.owner,
     repo: context.payload.inputs.repo,
     ref: context.payload.inputs.ref
@@ -12,22 +12,22 @@ module.exports = async ({ core, context }) => {
   core.debug(input)
 
   // Verify that repo exists in the config
-  const repo = Object.values(source.get(input.owner)).find(source => source.name == input.repo)
-  if (!repo) {
-    return core.setFailed(`Config not found for source ${input.owner}/${input.repo} with ref ${input.ref}`)
+  const repo = Object.values(config.get(input.owner)).find(repo => repo.name === input.repo)
+  if (repo === undefined) {
+    return core.setFailed(`Config not found for repo ${input.owner}/${input.repo}`)
   }
 
   // Validate that ref is in expected format
   const validateRefs = repo.refs.some(r => {
-    if (r.type == "regex") {
+    if (r.type === "regex") {
       const regex = new RegExp(r.ref)
       return regex.test(input.ref)
-    } else if (r.type == "branch") {
-      return r.ref == input.ref
+    } else if (r.type === "branch") {
+      return r.ref === input.ref
     }
   })
   if (!validateRefs) {
-    return core.setFailed(`Ref ${input.ref} is not allowed for source ${input.owner}/${input.repo}`)
+    return core.setFailed(`Ref ${input.ref} is not allowed for repo ${input.owner}/${input.repo}`)
   }
 
   core.exportVariable('REPO', input.repo)
